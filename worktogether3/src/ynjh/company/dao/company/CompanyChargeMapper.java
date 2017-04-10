f6d7882a7ccb52192a6ecf90d8a279b8d69b10ce
ly <ly@DESKTOP-R41TCJ6>
2017-04-10 17:51:45 +0800

    李胤修改了company实体类，添加了companyIntroction、companyCharge实体类 添加了companyIntService,修改了CompanyService,添加CompanyServiceImpl,修改CompanyServiceImpl 添加CompanyController,修改CompanyIntController
    晏梓炫
    修改CompanyRecruitMapper、CompanyRecruitController
    
    黄冰雁
    基本完成。

--- worktogether3/src/ynjh/company/dao/company/CompanyChargeMapper.java
+++
@@ -1,8 +0,0 @@
-package ynjh.company.dao.company;
-
-import org.apache.ibatis.annotations.Param;
-
-public interface CompanyChargeMapper {
-	public Integer updateCompanyrMoney(@Param("userMoney")Integer companyMoney,@Param("companyId")Integer companyId);
-
-}
--- worktogether3/src/ynjh/company/dao/company/CompanyIntrodutionMapper.java
+++
@@ -1,7 +0,0 @@
-package ynjh.company.dao.company;
-
-import ynjh.company.entity.Company;
-
-public interface CompanyIntrodutionMapper {
-	public Integer updateCompanyInt(Company company);
-}
