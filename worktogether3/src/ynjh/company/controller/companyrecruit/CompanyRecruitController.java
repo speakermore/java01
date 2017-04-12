dd0c2dd1e3e41c99f36493e76fc9c3fcc2b9e0dd
ly <ly@DESKTOP-R41TCJ6>
2017-04-12 09:34:36 +0800

    改错，一个小错

--- worktogether3/src/ynjh/company/controller/companyrecruit/CompanyRecruitController.java
+++ worktogether3/src/ynjh/company/controller/companyrecruit/CompanyRecruitController.java
@@ -11,14 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-
-
+import ynjh.company.entity.Company;
 import ynjh.company.entity.CompanyRecruit;
 import ynjh.company.service.CompanyRecruitService;
-
-
 @Controller
-@RequestMapping("/companyRecruit")
+@RequestMapping("/company")
 public class CompanyRecruitController {
 	@Resource
 	private CompanyRecruitService companyRecruitService;
@@ -26,28 +23,33 @@ public class CompanyRecruitController {
 	public String addCompanyRecuit(){
 		return "addCompanyRecruit";
 	}
+	@RequestMapping(value={"/index","/"},method=RequestMethod.GET)
+	public String index(){
+		return "/company/index";
+	}
 	@RequestMapping(value="/addCompanyRecruit", method=RequestMethod.POST)
 	public ModelAndView addCompanyRecruit(CompanyRecruit companyRecruit,HttpSession session){
-		CompanyRecruit user=(CompanyRecruit)session.getAttribute("companyId");
-		companyRecruit.setId(user.getId());
+		Company company=(Company)session.getAttribute("company");
+		companyRecruit.setCompanyId(company.getId());
 		companyRecruit.setCmpRecTime(new Timestamp(System.currentTimeMillis()));
 		int result=companyRecruitService.addCompanyRecruit(companyRecruit);
 		ModelAndView mv=new ModelAndView();
 		if(result>0){
 			mv.addObject("operatorInfo","文章添加成功");
 			mv.addObject("toPage","companyRecruit/findAll");
-			mv.setViewName("info");
+			mv.setViewName("/company/info");
 		}else{
 			mv.addObject("operatorInfo", "文章添加失败");
-			mv.setViewName("redirect:/addCompanyRecruit");
+			mv.setViewName("redirect:/company/addCompanyRecruit");
 		}
 		return mv;
 	}
+	
 	 @RequestMapping(value="/findAll")
 	 public ModelAndView findAll(){
-		 List<CompanyRecruit> companyRecruit=companyRecruitService.findAll(null);
+		 List<CompanyRecruit> companyRecruits=companyRecruitService.findAll(null);
 		 ModelAndView mv=new ModelAndView("index");
-		 mv.addObject("companyRecruits", companyRecruit);
+		 mv.addObject("companyRecruits", companyRecruits);
 		 return mv;
 	 }
 	 @RequestMapping(value="findById")
@@ -55,21 +57,21 @@ public class CompanyRecruitController {
 		 CompanyRecruit companyRecruit=companyRecruitService.findById(id);
 		 ModelAndView mv=new ModelAndView();
 		 mv.addObject("companyRecruit",companyRecruit );
+		 mv.setViewName(topage);
 		 return mv;
 	 }
-	 @RequestMapping(value="hidden")
+	 @RequestMapping("/hidden")
 	 public ModelAndView hidden(Integer id){
 			int result=companyRecruitService.hidden(id);
 			ModelAndView mv=new ModelAndView();
 			if(result>0){
 				mv.addObject("operatorInfo","修改成功");
 				mv.addObject("toPage","findAll");
-				mv.setViewName("info");
+				mv.setViewName("/company/info");
 			}else{
 				mv.addObject("operatorInfo","修改失败");
 				mv.addObject("toPage","addCompanyRecruit");
-				
-				mv.setViewName("info");
+				mv.setViewName("/company/info");
 			}
 			return mv;
 }
