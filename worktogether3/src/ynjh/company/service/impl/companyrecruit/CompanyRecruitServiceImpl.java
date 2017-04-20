636e2a48c3a09aa7bd982f11c244588ddf48b889
mouyong <mouyong1973@yeah.net>
2017-04-20 09:07:30 +0800

    牟勇：AdminController说它被改了，所以提交改变。

--- worktogether3/src/ynjh/company/service/impl/companyrecruit/CompanyRecruitServiceImpl.java
+++ worktogether3/src/ynjh/company/service/impl/companyrecruit/CompanyRecruitServiceImpl.java
@@ -1,53 +1,3 @@
-<<<<<<< HEAD
-package ynjh.company.service.impl.companyrecruit;
-
-import java.util.List;
-
-import javax.annotation.Resource;
-
-import org.apache.log4j.Logger;
-import org.springframework.stereotype.Service;
-
-
-
-import ynjh.company.dao.companyrecruit.CompanyRecruitMapper;
-import ynjh.company.entity.CompanyRecruit;
-import ynjh.company.service.CompanyRecruitService;
-@Service
-public class CompanyRecruitServiceImpl implements CompanyRecruitService {
-	private Logger logger=Logger.getLogger(this.getClass());
-	@Resource
-	private CompanyRecruitMapper companyRecruitMapper;
-	@Override
-	public int addCompanyRecruit(CompanyRecruit companyRecruit) {
-		int result=-1;
-		try {
-			return companyRecruitMapper.addCompanyRecruit(companyRecruit);
-		} catch (Exception e) {
-			e.printStackTrace();
-			logger.warn("数据库添加文章失败");
-		}
-		return result;
-	}
-
-	@Override
-	public List<CompanyRecruit> findAll(Integer page) {
-		return companyRecruitMapper.findAll(page);
-	}
-
-	@Override
-	public CompanyRecruit findById(Integer id) {
-		return companyRecruitMapper.findById(id);
-	}
-
-	@Override
-	public int hidden(Integer id) {
-		return companyRecruitMapper.hidden(id);
-
-}
-}
-
-=======
 package ynjh.company.service.impl.companyrecruit;
 
 import java.util.List;
@@ -118,4 +68,3 @@ public class CompanyRecruitServiceImpl implements CompanyRecruitService {
 	}
 
 }
->>>>>>> e8000c9b5ff629cb3b251d9968679f5bfdefef3a
