5543cd9377c801d91489f5fec65e8f0d170aa034
朱吉 <mouyong1973@yeah.net>
2017-05-17 17:17:47 +0800

    朱吉-2017-05-17 17:16:32 再次更新了auditingResume.jsp页面，在CommonStatus.java种添加了技能水平的静态量

--- worktogether3/src/ynjh/common/util/CommonStatus.java
+++ worktogether3/src/ynjh/common/util/CommonStatus.java
@@ -17,4 +17,12 @@ public class CommonStatus {
 	public static final String[] COMPANY_TYPE={"","政府机构","国有企业","民营企业","其他"};
 	//企业规模
 	public static final String[] COMPANY_SIZE={"","20人","50人","80人","100人","150人","200人","200人以上"};
+	//外语水平
+	public static final String[] FL_ABILITY={"","入门","一般","熟练","精通"};
+	//外语类型
+	public static final String[] FL_TYPE={"","英语","日语","德语","......"};
+	//外语等级
+	public static final String[] ENGLISH_GRADE={"","国家英语考试3级","国家英语考试4级","国家英语考试6级","专业4级","专业8级","未参加","未通过"};
+	//计算机水平
+	public static final String[] COMPUTER_ABILITY={"","入门","一般","熟练","精通"};
 }
